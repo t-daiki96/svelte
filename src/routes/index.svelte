8cd9ea73cb4c4476bad11fb45abf9685ab7e1f3f
t-daiki96 <t.daiki96@gmail.com>
2021-12-31 18:01:51 +0900

    todoリスト create read update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,14 +2,48 @@
 	export const prerender = true;
 </script>
 
-<script>
-	let text = 'world';
+<script lang="ts">
+	let uid = 1;
+	let todos = [
+		{
+			id: uid++,
+			done: false,
+			description: 'やること1'
+		},
+		{
+			id: uid++,
+			done: false,
+			description: 'やること2'
+		}
+	];
+	const add = (target: EventTarget) => {
+		if (!(target instanceof HTMLInputElement)) {
+			return;
+		}
+		todos = [...todos, { id: uid++, done: false, description: target.value }];
+		target.value = '';
+	};
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>hello {text}</h1>
-</section>
+<h2>input</h2>
+<input on:keydown={(e) => e.key === 'Enter' && add(e.target)} />
+
+<h2>todo</h2>
+{#each todos.filter((todo) => !todo.done) as todo (todo.id)}
+	<label>
+		<input type="checkbox" on:change={() => (todo.done = true)} />
+		{todo.description}
+	</label>
+{/each}
+
+<h2>done</h2>
+{#each todos.filter((todo) => todo.done) as todo (todo.id)}
+	<label>
+		<input type="checkbox" checked on:change={() => (todo.done = false)} />
+		{todo.description}
+	</label>
+{/each}
